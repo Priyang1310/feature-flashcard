c70a363811a73c09b460cbb207684256b2c8f8c9
Priyang1310 <consistentcoderpriyangdesai@gmail.com>
2024-08-12 13:05:30 +0530

    Final Commit

--- backend/config/database.js
+++ backend/config/database.js
@@ -1,9 +1,11 @@
-// config/database.js
-const { Sequelize } = require('sequelize');
+const mysql = require('mysql2/promise');
 
-const sequelize = new Sequelize('flashcard_db', 'flashcard_user', 'your_password', {
-  host: 'localhost',
-  dialect: 'mysql',
+const pool = mysql.createPool({
+  host: 'http://sql12.freesqldatabase.com/',
+  user: 'sql12725420',
+  password: 'L68yrVi1S2',
+  database: 'sql12725420',
+  port:3306
 });
 
-module.exports = sequelize;
+module.exports = pool;
--- backend/index.js
+++ backend/index.js
@@ -1,54 +1,48 @@
-// index.js
 const express = require("express");
 const bodyParser = require("body-parser");
 const cors = require("cors");
-const sequelize = require("./config/database");
-const Flashcard = require("./models/Flashcard.model");
+const pool = require("./config/database");
 
 const app = express();
 app.use(cors());
 app.use(bodyParser.json());
 
-// Sync Database
-sequelize.sync().then(() => {
-  console.log("Database & tables created!");
-});
-
 // Get All Flashcards
 app.get("/api/flashcards", async (req, res) => {
-  const flashcards = await Flashcard.findAll();
-  res.json(flashcards);
+  const [rows] = await pool.query("SELECT * FROM Flashcards");
+  res.json(rows);
 });
 
 // Get Flashcard by ID
 app.get("/api/flashcards/:id", async (req, res) => {
-  const flashcard = await Flashcard.findByPk(req.params.id);
-  res.json(flashcard);
+  const [rows] = await pool.query("SELECT * FROM Flashcards WHERE id = ?", [req.params.id]);
+  res.json(rows[0]);
 });
 
 // Create New Flashcard
 app.post("/api/flashcards", async (req, res) => {
   const { question, answer } = req.body;
-  const newFlashcard = await Flashcard.create({ question, answer });
+  const [result] = await pool.query("INSERT INTO Flashcards (question, answer) VALUES (?, ?)", [question, answer]);
+  const newFlashcard = {
+    id: result.insertId,
+    question,
+    answer
+  };
   res.json(newFlashcard);
 });
 
 // Update Flashcard
 app.put("/api/flashcards/:id", async (req, res) => {
   const { question, answer } = req.body;
-  await Flashcard.update(
-    { question, answer },
-    { where: { id: req.params.id } }
-  );
+  await pool.query("UPDATE Flashcards SET question = ?, answer = ? WHERE id = ?", [question, answer, req.params.id]);
   res.json({ message: "Flashcard updated" });
 });
 
 // Delete Flashcard
 app.delete("/api/flashcards/:id", async (req, res) => {
-  await Flashcard.destroy({ where: { id: req.params.id } });
+  await pool.query("DELETE FROM Flashcards WHERE id = ?", [req.params.id]);
   res.json({ message: "Flashcard deleted" });
 });
-
 
 const PORT = 5000;
 
