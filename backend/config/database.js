051d3496fa77f2553d0bc36a37646c869ad07e27
Priyang1310 <consistentcoderpriyangdesai@gmail.com>
2024-08-12 13:07:04 +0530

    Final Commit

--- backend/config/database.js
+++ backend/config/database.js
@@ -6,6 +6,7 @@ const pool = mysql.createPool({
   password: 'L68yrVi1S2',
   database: 'sql12725420',
   port:3306
+});
 const sequelize = new Sequelize('flashcard_db', 'flashcard_user', 'your_password', {
   host: 'mysql-2e66b442-consistentcoderpriyangdesai-7606.k.aivencloud.com',
   dialect: 'mysql',
